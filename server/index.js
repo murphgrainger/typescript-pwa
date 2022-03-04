cb258eff4abc27c24a3845cf10f9cfeb07d1e4da
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2022-03-04 15:23:52 -0700

    updated server file to load client static files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/ping', (req, res) => {
     res.json({ message: 'Ping ping ping!'});
