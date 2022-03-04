e668622e5e9c8857b1b20f1b80808e069c269632
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2022-03-04 15:37:01 -0700

    added missing static script to server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/ping', (req, res) => {
